905abe9210d356588814b33e6fe50b45675b2986
IkoAfianando <ikoafianando123@gmail.com>
2022-05-30 09:50:52 +0700

    Add: Function

---
+++ src/main/kotlin/Function.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val user = setUser("Iko Afianando", 17)
+    println(user)
+
+    print("Hello Iko")
+}
+
+fun setUser(name: String, age: Int){
+    print("My Name is $name and i'am $age years Old")
+}
