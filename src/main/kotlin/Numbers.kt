cf47a06c43a396f697326bf41f5783a462b7f1cb
IkoAfianando <ikoafianando123@gmail.com>
2022-05-31 09:36:22 +0700

    Update -> Numbers.kt

---
+++ src/main/kotlin/Numbers.kt
@@ -0,0 +1,67 @@
+fun main() {
+    integer()
+    long()
+    short()
+    byteNumber()
+    konversi()
+    double()
+    float()
+}
+
+fun integer() {
+    val maxInt = Int.MAX_VALUE
+    val minInt = Int.MIN_VALUE
+
+    println("Maksimum Integer $maxInt")
+    println("Minimum Integer $minInt")
+}
+
+fun long() {
+    val maxLong = Long.MAX_VALUE
+    val minLong = Long.MIN_VALUE
+
+    println("Maksimum Long $maxLong")
+    println("Minimum Long $minLong")
+}
+
+fun short() {
+    val maxShort = Short.MAX_VALUE
+    val minShort = Short.MIN_VALUE
+
+    println("Maksimum Short $maxShort")
+    println("Minimum Short $minShort")
+}
+
+fun byteNumber() {
+    val maxByte = Byte.MAX_VALUE
+    val minByte = Byte.MIN_VALUE
+
+    println("Maksimum Byte $maxByte")
+    println("Minimum Byte $minByte")
+}
+
+fun double() {
+    val maxDouble = Double.MAX_VALUE
+    val minDouble = Double.MIN_VALUE
+
+    println("Maksimum Double $maxDouble")
+    println("Minimum Double $minDouble")
+}
+
+fun float() {
+    val maxFloat = Float.MAX_VALUE
+    val minFloat = Float.MIN_VALUE
+
+    println("Maksimum Float $maxFloat")
+    println("Minimum Float $minFloat")
+}
+
+fun konversi() {
+//    toByte(): Byte
+//    toShort(): Short
+//    toInt(): Int
+//    toLong(): Long
+//    toFloat(): Float
+//    toDouble(): Double
+//    toChar(): Char
+}
