a91ab7f6eed8f67a68af6b515ffbabc4d4e5253b
IkoAfianando <ikoafianando123@gmail.com>
2022-05-31 10:55:40 +0700

    Update-> StringTemplate.kt

---
+++ src/main/kotlin/StringTemplate.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val name = "Iko Afianando"
+    val umur = 17
+    println("Hello, My Name is $name, I'm $umur year's old")
+
+    val hour = 7
+    println("Office ${if (hour > 7) "already close" else "is open"} ")
+}
